8f68ef4f3b79c26227cd940071765d0abb3f7ffc
Russ Meyerriecks <datachomper@gmail.com>
2010-08-10 16:52:53 -0500

    wip, cant get multidimensional lists to work, bleh

--- sim.py
+++ sim.py
@@ -1,20 +1,60 @@
 #!/usr/bin/python
-class Boxes:
-	'''Stores the season's box data'''
-	def __init__(self):
-		self.week = None
+class Season:
+	"""Stores the season's box data for processing by strategies"""
+
+	def __init__(self, data):
+		# Grep the csv data, this may need to
+		# be abstracted if we use a DB later
+		self.numweeks = 0
+		for line in data:
+			if not line[0].isdigit():
+				continue
+			if int(line[0]) > self.numweeks:
+				self.numweeks = int(line[0])
+		self.week = [None] * self.numweeks
+
+		for line in data:
+			# Ignore comments
+			if not line[0].isdigit():
+				continue
+			game = Game()
+			game.week = int(line[0])
+			game.winner = line[5]
+			game.loser = line[7]
+			if line[6] == '@':
+				game.home = line[7]
+				game.visitor = line[5]
+			else:
+				game.home = line[5]
+				game.visitor = line[7]
+			game.ptsw = int(line[8])
+			game.ptsl = int(line[9])
+			if line[14] == '':
+				game.spread = None
+			else:
+				game.spread = float(line[14])
+
+			self.week[game.week] = [None] * 3
+
+	def __iter__(self):
+		return self
+# season.week[1].game[1]
+
+class Game:
+	pass
 
 class Simulator:
-	'''Runs each prediction strategy, displays results'''
+	"""Runs each prediction strategy, displays results"""
 	def __init__(self, file):
 		import csv
 		self.strategies = []
 		self.weeks = 16
 		# Import csv season data and strip comments
-		self.data = csv.reader(open(file, 'r'), delimiter=',')
+		self.data = Season(csv.reader(open(file, 'r'), delimiter=','))
 
 	def register(self, strategy):
 		self.strategies.append(strategy)
+		strategy.load(self.data)
 
 	def weekly_totals(self):
 		# Find "names" column width
@@ -40,9 +80,16 @@ class Simulator:
 			print result
 
 class Strategy:
-	'''Scaffolding class for prediction strategies'''
+	"""Scaffolding class for prediction strategies"""
 	def __init__(self):
 		self.name = ""
+		self.data = None
+
+	def load(self, data):
+		self.data = data
+	
+	def run(self):
+		pass
 
 	class result:
 		def __init__(self):
@@ -50,11 +97,16 @@ class Strategy:
 			self.total = 0
 
 class WithSpread(Strategy):
-	'''Bet with the spread for every game'''
+	"""Bet with the spread for every game"""
 	def __init__(self):
 		self.name = "With Spread"
 		self.result.week = [0,100,200,1000,4000,1000,-200,-56,1000,1000,10,555,13,140,15,1600]
 		self.result.total = sum(self.result.week)
+
+	def run(self):
+		for week in self.data.weeks:
+			for game in week:
+				print "%s vs %s" % (game.home, game.visitor)
 
 # Bootstrap
 sim = Simulator('2009.csv')
