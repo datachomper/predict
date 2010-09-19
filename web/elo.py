cf6cfd5b202d25f5289a7c469a5a028c06170d2a
Russ Meyerriecks <datachomper@gmail.com>
2010-09-19 17:47:47 -0500

    First reliable, outputtable spread predictions for 2010

--- web/elo.py
+++ web/elo.py
@@ -6,6 +6,7 @@ setup_environ(settings)
 from data.models import Box
 from numpy import std, average
 import sys
+import types
 
 tally = {}
 delta = []
@@ -27,7 +28,7 @@ class Stat():
 		self.rate = 1500
 
 # Grab year argument to the script
-year = 2009
+year = 2010
 for arg in sys.argv[1:]:
 	year = arg
 	break
@@ -46,9 +47,6 @@ for week in range(1, num_weeks_avail+1):
 		if not match.road in tally:
 			tally[match.road] = Stat()
 
-		# Print match info
-		print "%s:%d vs %s:%d" % (match.home, match.hscore, match.road, match.rscore)
-
 		# The Casino's are much smarter than I am so we pre-seed our ratings
 		# by reverse engineering their spread predictions
 		if week == 1:
@@ -57,9 +55,18 @@ for week in range(1, num_weeks_avail+1):
 
 		# Find accuracy of curent ELO
 		prediction = (tally[match.road].rate - tally[match.home].rate)*7/100
+
+		# Find out if we have final score data for this week/match
+		if type(match.hscore) is types.NoneType:
+			print "%s vs %s : %d" % (match.home, match.road, prediction)
+			continue
+
+		# If we have the scores, calculate the results
 		diff = match.rscore - match.hscore
 		d = distance(prediction, diff)
 
+		# Print match info
+		print "%s:%d vs %s:%d" % (match.home, match.hscore, match.road, match.rscore)
 		print " > pre-game:  home: %d road: %d" % (tally[match.home].rate, tally[match.road].rate)
 		print " > predicted %d, spread %d, actual %d, delta %d" % (prediction, match.line, diff, d)
 		if week > 1:
@@ -75,4 +82,4 @@ for week in range(1, num_weeks_avail+1):
 		print " > post-game: home: %d road: %d" % (tally[match.home].rate, tally[match.road].rate)
 
 print "------"
-print "Accuracy: %d Std Dev: %d" % (average(delta), std(delta))
+print "Accuracy: %f Std Dev: %f" % (average(delta), std(delta))
