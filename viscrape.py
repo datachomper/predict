3dc1caa6d17ad5b74568915849304d9190a78b8b
Russ Meyerriecks <datachomper@gmail.com>
2010-09-20 19:51:05 -0500

    Fully functional scraper!
    Output needs to be modified or the script should be turned into a class

--- viscrape.py
+++ viscrape.py
@@ -1,11 +1,47 @@
 #!/usr/bin/python
 
-from lxml.html import parse
-import urllib
-import StringIO
+# Scrapes vegasinsider.com for NFL lines, scores, and matchups
 
-doc = parse('http://www.vegasinsider.com/nfl/scoreboard/scores.cfm/week/2/season/2010').getroot()
-print type(doc)
-# <td class="sportsPicksBorder">
-for table in doc.cssselect('span.sub_title_red'):
-	print table.text_content()
+from BeautifulSoup import BeautifulSoup
+import urllib2
+
+year = 2010
+week = 2
+
+page = urllib2.urlopen('http://www.vegasinsider.com/nfl/scoreboard/scores.cfm/week/%d/season/%d'%(week,year))
+soup = BeautifulSoup(page)
+
+for box in soup('td', 'sportPicksBorder'):
+	out = []
+
+	# Find the (Abbreviated) teams in the game
+	road = box.table.tbody.findAll('tr')[3].findAll('td')[0].a.string
+	home = box.table.tbody.findAll('tr')[4].findAll('td')[0].a.string
+	out.append(home)
+	out.append(road)
+
+	# Find the odds and determine which is the line
+	rline = box.table.tbody.findAll('tr')[3].findAll('td')[1].string.strip('&nbsp;')
+	hline = box.table.tbody.findAll('tr')[4].findAll('td')[1].string.strip('&nbsp;')
+
+	# They put both the line and the over/under in random places
+	# Assuming the over/under is always greater than the line, we take
+	# the smallest to be the line
+	line = -float(rline) if (abs(float(rline)) < abs(float(hline))) else float(hline)
+	out.append(line)
+
+	# Grab the scores if the games have been completed
+	status = box.find('span', 'sub_title_red').contents[0].strip()
+	if status == "Final Score":
+		try:
+			rscore = int(box.table.tbody.findAll('tr')[3].findAll('td')[6].font.b.string.strip().strip('&nbsp;'))
+			hscore = int(box.table.tbody.findAll('tr')[4].findAll('td')[6].font.b.string.strip().strip('&nbsp;'))
+		except AttributeError:
+			# Seems to be safe to ignore this error for now I think it's a false positive
+			# print box.table.tbody.findAll('tr')[3].findAll('td')[6]
+			pass
+
+		out.append(hscore)
+		out.append(rscore)
+
+	print out
